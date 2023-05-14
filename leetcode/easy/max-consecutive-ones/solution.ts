06838acab3714954da3442b48be155c4d6c6507f
Dillon Brock <mdillon1297@gmail.com>
2023-05-13 22:37:09 -0700

    java and typescript solutions max consecutive ones

---
+++ leetcode/easy/max-consecutive-ones/solution.ts
@@ -0,0 +1,17 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let currentCount: number = 0;
+  let maxCount: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
+          currentCount++;
+      }
+      else currentCount = 0;
+      console.log(currentCount);
+      if (maxCount < currentCount) {
+          maxCount = currentCount;
+      }
+  }
+
+  return maxCount;
+};
