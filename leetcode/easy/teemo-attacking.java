bf00651893f27595c6503c5ac1d3e3ae145e779c
Dillon Brock <mdillon1297@gmail.com>
2023-05-14 23:42:21 -0700

    java solution teemo attacking

---
+++ leetcode/easy/teemo-attacking.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        for (int i = 0; i < timeSeries.length; i++) {
+            if (i == timeSeries.length - 1) {
+                total += duration;
+                break;
+            }
+            if (timeSeries[i + 1] - timeSeries[i] < duration) {
+                total += timeSeries[i + 1] - timeSeries[i];
+            } else {
+                total += duration;
+            }
+        }
+        return total;
+    }
+}
